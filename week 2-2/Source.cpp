0f499347d79e9d68c3fca75bff5e981a8620b91f
PENPITCHA SAPPAKUN <63010709@KMITL.AC.TH>
2020-08-21 17:00:37 +0700

    1

--- week 2-2/Source.cpp
+++ week 2-2/Source.cpp
@@ -5,15 +5,15 @@ int main()
 	scanf_s("%d", &x);
 	scanf_s("%d", &y);
 
-	if (x > y)
+	if (x < y)
 	{
 		while (m <= y)
 		{
 			if (x % m == 0 && y % m == 0)
 			{
-				m = 1 * m;
+				m = 1 * m;  	
 			}
-			m++;
+			m++;	
 		}
 		printf("%d ", m - 1);
 	}
