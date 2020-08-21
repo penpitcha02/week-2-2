a2d833464405ca71d552ae6fa8e4d3fdf8d11092
PENPITCHA SAPPAKUN <63010709@KMITL.AC.TH>
2020-08-21 16:46:22 +0700

    Add project files.

---
+++ week 2-2/Source.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+	int x, y, m = 2, n = 2;
+	scanf_s("%d", &x);
+	scanf_s("%d", &y);
+
+	if (x > y)
+	{
+		while (m <= y)
+		{
+			if (x % m == 0 && y % m == 0)
+			{
+				m = 1 * m;
+			}
+			m++;
+		}
+		printf("%d ", m - 1);
+	}
+	else
+	{
+		while (n <= x)
+		{
+			if (y % n == 0 && x % n == 0)
+			{
+				n = 1 * n;
+			}
+			n++;
+		}
+		printf("%d ", n - 1);
+	}
+	return 0;
+}
